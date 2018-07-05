117cad7b72ef1929df31f74fbd2824fc686ee23b
Annamary Cartagena <anitac@fb.com>
2018-07-05 13:51:14 -0700

    making activity menu_main to compose tweet, connected to ComposeActivity.java

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -1,13 +1,31 @@
 package com.codepath.apps.restclienttemplate;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
 
 public class ComposeActivity extends AppCompatActivity {
+
+    Button button = (Button) findViewById(R.id.tweetbutton);
+    EditText editText = (EditText) findViewById(R.id.composeTweet);
+    private TwitterClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                client.sendTweet(editText.getText(), new JsonHttpResponseHandler());
+            }
+        });
     }
+
+
+
 }
--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,10 +1,13 @@
 package com.codepath.apps.restclienttemplate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -94,4 +97,24 @@ public class TimelineActivity extends AppCompatActivity {
         });
     }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.miCompose:
+                Intent intent = new Intent(this, ComposeActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.miProfile:
+            //    showProfileView();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
